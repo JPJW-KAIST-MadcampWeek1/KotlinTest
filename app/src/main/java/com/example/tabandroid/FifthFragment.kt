f59f42086f7caad80277532a68730a95ce52b16d
jjpark51 <jjpark57@hotmail.com>
2024-01-01 21:03:22 +0900

    Finished quiz app

--- app/src/main/java/com/example/tabandroid/FifthFragment.kt
+++ app/src/main/java/com/example/tabandroid/FifthFragment.kt
@@ -28,13 +28,29 @@ class FifthFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private val questions = arrayOf("What team does Shohei Ohtani play for?",
-        "What is Jaewon's MBTI?", "What is Jinsuk's dream car?")
+//    private val questions = arrayOf("What team does Shohei Ohtani play for?",
+//        "What is Jaewon's MBTI?", "What is Jinsuk's dream car?")
 
-    private val options = arrayOf(arrayOf("New York Yankees", "Los Angeles Angels", "Los Angeles Dodgers"),
-        arrayOf("ENTP", "ESFP", "ENFP"), arrayOf("Genesis G90", "BMW i7", "Porsche Taycan"))
+    private val questions =
+        arrayOf("Which of the following sorting algorithms is the fastest for sorting small arrays?",
+            "Merge Sort uses which of the following algorithm to implement sorting"
+        ,"Which algorithm technique does Fibonacci search use?",
+            "What is the advantage of selection sort over other Sorting Techniques?",
+            "Which of the following is not true about QuickSort?",
+        "Which of the following sorting algorithm is only applicable to positive integers",
+            "Which of the following traversal in a BST results in a sorted output?",
+        "What is the worst case time complexity of a quick sort algorithm")
 
-    private val correctAnswers = arrayOf(2, 1, 2)
+    private val options = arrayOf(arrayOf("Insertion Sort", "Shell Sort", "Quick Sort", "Heap Sort"),
+        arrayOf("Divide and Conquer", "Greedy Algorithm", "Backtracking", "Dynamic Programming"),
+        arrayOf("Brute Force", "Divide and Conquer", "Greedy Technique", "Backtracking"),
+    arrayOf( "It works best for inputs which are already sorted", "It is faster than any other sorting technique", "It is scalable","It requires no additional storage space",),
+    arrayOf("Adaptive sorting algorithm", "in-place algorithm", "pivot position can be changed", "can be implemented as a stable sort"),
+    arrayOf("Quick Sort", "Strand Sort", "Bead Sort", "Heap Sort"),
+        arrayOf("In order Traversal", "Post Order Traversal", "Pre order traversal", "Breadth First Traversal"),
+    arrayOf("O(NlogN)", "O(N²)","O(logN","O(N)"))
+
+    private val correctAnswers = arrayOf(0, 0, 1, 3, 3, 2, 0, 1 )
 
     private var currentQuestionIndex = 0
     private var score = 0
@@ -64,6 +80,9 @@ class FifthFragment : Fragment() {
             checkAnswer(1)
         }
         binding.ThirdButton.setOnClickListener {
+            checkAnswer(2)
+        }
+        binding.FourthButton.setOnClickListener {
             checkAnswer(2)
         }
 
@@ -100,6 +119,7 @@ class FifthFragment : Fragment() {
             0 -> binding.FirstButton.setBackgroundColor(Color.GREEN)
             1 -> binding.SecondButton.setBackgroundColor(Color.GREEN)
             2 -> binding.ThirdButton.setBackgroundColor(Color.GREEN)
+            3 -> binding.FourthButton.setBackgroundColor(Color.GREEN)
         }
     }
 
@@ -108,6 +128,7 @@ class FifthFragment : Fragment() {
             0 -> binding.FirstButton.setBackgroundColor(Color.RED)
             1 -> binding.SecondButton.setBackgroundColor(Color.RED)
             2 -> binding.ThirdButton.setBackgroundColor(Color.RED)
+            3 -> binding.FourthButton.setBackgroundColor(Color.RED)
         }
     }
 
@@ -115,6 +136,7 @@ class FifthFragment : Fragment() {
         binding.FirstButton.setBackgroundColor(Color.rgb(50,59,96))
         binding.SecondButton.setBackgroundColor(Color.rgb(50,59,96))
         binding.ThirdButton.setBackgroundColor(Color.rgb(50,59,96))
+        binding.FourthButton.setBackgroundColor(Color.rgb(50,59,96))
     }
 
 //    private fun showResults() {
@@ -126,8 +148,7 @@ class FifthFragment : Fragment() {
 
 
         binding.quizTitle.visibility = View.VISIBLE
-        binding.quizImage.visibility = View.VISIBLE
-//        binding.codinganim.visibility = View.VISIBLE
+        binding.codingdude.visibility = View.VISIBLE
         binding.startButton.visibility = View.VISIBLE
         binding.resultText.visibility = View.GONE
         binding.progressCircle.visibility = View.GONE
@@ -135,23 +156,28 @@ class FifthFragment : Fragment() {
         binding.FirstButton.visibility = View.GONE
         binding.SecondButton.visibility = View.GONE
         binding.ThirdButton.visibility = View.GONE
+        binding.FourthButton.visibility = View.GONE
         binding.restartButton.visibility = View.GONE
+        binding.guyAnimation.playAnimation()
+
     }
 
     private fun displayQuestions() {
         binding.quizTitle.visibility = View.GONE
-        binding.quizImage.visibility = View.GONE
+        binding.codingdude.visibility = View.GONE
         binding.startButton.visibility = View.GONE
         binding.questionText.visibility = View.VISIBLE
         binding.FirstButton.visibility = View.VISIBLE
         binding.SecondButton.visibility = View.VISIBLE
         binding.ThirdButton.visibility = View.VISIBLE
+        binding.FourthButton.visibility = View.VISIBLE
         binding.restartButton.visibility = View.GONE
         // This shows what questions are going to be displayed
         binding.questionText.text = questions[currentQuestionIndex]
         binding.FirstButton.text = options[currentQuestionIndex][0]
         binding.SecondButton.text = options[currentQuestionIndex][1]
         binding.ThirdButton.text = options[currentQuestionIndex][2]
+        binding.FourthButton.text = options[currentQuestionIndex][3]
         resetButtonColors()
 
     }
@@ -204,7 +230,7 @@ class FifthFragment : Fragment() {
         binding.FirstButton.visibility = View.GONE
         binding.SecondButton.visibility = View.GONE
         binding.ThirdButton.visibility = View.GONE
-
+        binding.FourthButton.visibility =  View.GONE
         // update the resultView with the result details
 
     }
