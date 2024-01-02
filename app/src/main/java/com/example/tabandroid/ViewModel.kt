79c3bc8c14b5d30e5c2b5173e6f31426833be1e3
jjpark51 <jjpark57@hotmail.com>
2024-01-02 22:43:36 +0900

    fixed menu bar cut

--- app/src/main/java/com/example/tabandroid/ViewModel.kt
+++ app/src/main/java/com/example/tabandroid/ViewModel.kt
@@ -12,9 +12,49 @@ class ImageViewModel(application: Application) : AndroidViewModel(application) {
     init {
         // Example: Adding drawables to imagesList
         val drawableResources = listOf(
-            R.drawable.cat1,
+//            R.drawable.cat1,
             R.drawable.cat2,
-            R.drawable.cat3,
+//            R.drawable.cat3,
+//            R.drawable.cat4,
+//            R.drawable.cat5,
+//            R.drawable.cat6,
+//            R.drawable.cat7,
+//            R.drawable.cat8,
+//            R.drawable.cat9,
+//            R.drawable.cat10,
+//            R.drawable.cat11,
+//            R.drawable.cat12,
+//            R.drawable.cat13,
+//            R.drawable.cat14,
+//            R.drawable.cat15,
+//            R.drawable.cat16,
+//            R.drawable.cat17,
+//            R.drawable.cat18,
+//            R.drawable.cat19,
+//            R.drawable.cat20,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+            R.drawable.cat2,
+
+
+
+
             // Add more drawable resource IDs here
         )
 
